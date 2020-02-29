44e70b80a35a676d6e441027862c0370ca1b25ba
global jasamant <jasamant.sikarwar@opstree.com>
2020-02-29 12:56:22 +0530

    added

--- vars/pipeline-library.groovy
+++ vars/pipeline-library.groovy
@@ -2,7 +2,7 @@ def call(String branch, String credentialsId, String url){
 
                 git branch: "${branch}"
                 credentialsId: "${credentialsId}"
-                url: "${https://github.com/jasamant2907/saleor.git}"
+                url: "${url}"
 }
 
 
